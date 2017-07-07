ebc128a98881b85227b5d46accd0358f1534709c
Felix Tilli <felixtilli1@gmail.com>
2017-07-08 00:08:39 +0200

    Added files

---
+++ src/CompressionHelper.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.IO.Compression;
+using System.Net.Http;
+using System.Web.Http.Filters;
+
+public class CompressionHelper
+{
+    public enum CompressionType
+    {
+        Deflate,
+        Gzip
+    }
+
+    public static HttpActionExecutedContext GetCompressedHttpActionExecutedContext(HttpActionExecutedContext context, CompressionType compressionType)
+    {
+        var content = new byte[0];
+
+        if (context.Response.Content != null)
+        {
+            var bytes = context.Response.Content.ReadAsByteArrayAsync().Result;
+
+            if (bytes != null)
+            {
+                content = CompressionHelper.Compress(bytes, compressionType, CompressionLevel.Fastest);
+            }
+        }
+
+        context.Response.Content = new ByteArrayContent(content);
+        context.Response.Content.Headers.Remove("Content-Type");
+        context.Response.Content.Headers.Add("Content-encoding", compressionType.ToString().ToLower());
+        context.Response.Content.Headers.Add("Content-Type", "application/json");
+
+        return context;
+    }
+
+    public static byte[] Compress(byte[] str, CompressionType compressionType, CompressionLevel compressionLevel)
+    {
+        using (var output = new MemoryStream())
+        {
+            var compressor = (Stream)null;
+
+            if (compressionType == CompressionType.Deflate)
+            {
+                compressor = new DeflateStream(output, compressionLevel);
+            }
+            else if (compressionType == CompressionType.Gzip)
+            {
+                compressor = new GZipStream(output, compressionLevel);
+            }
+
+            using (compressor)
+            {
+                compressor.Write(str, 0, str.Length);
+            }
+
+            return output.ToArray();
+        }
+    }
+}
---
+++ src/GzipCompressionAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Http.Filters;
+
+public class GzipCompressionAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuted(HttpActionExecutedContext context)
+    {
+        base.OnActionExecuted(CompressionHelper.GetCompressedHttpActionExecutedContext(context, CompressionHelper.CompressionType.Gzip));
+    }
+}
